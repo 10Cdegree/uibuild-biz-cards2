247d3c93f8c00fde21918d69d99eec3f4193bc83
10Cdegree <armin.szathmari@outlook.com>
2022-02-09 11:56:45 +0100

    :tada:

---
+++ uibuild-biz-cards2/src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+
+function App() {
+  return (
+    <body>
+      <div className="firstdiv">
+        <elements>
+          <h2>NotAProp</h2>
+          <h6>PizzaBar</h6>
+          <p>FirstAddress</p>
+        </elements>
+      </div>
+      <div className="seconddiv">
+        <elements>
+          <h2>NotAProp2</h2>
+          <h6>BarPizza</h6>
+          <p>SecondAddress</p>
+        </elements>
+      </div>
+      <div className="thirddiv">
+        <elements>
+          <h2>NotAProp3</h2>
+          <h6>McDonaldsInsanity</h6>
+          <p>ThirdAddress</p>
+        </elements>
+      </div>
+    </body>
+  );
+}
+
+export default App;
